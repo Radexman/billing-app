1d845a7ae470c55fa21b406587d9282b6d7d6cd4
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-11-04 21:23:01 +0100

    Error Handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,22 @@
-export const registerUser = (req, res) => {
-	res.send('Redister Route');
-};
+import expressAsyncHandler from 'express-async-handler';
 
-export const loginUser = (req, res) => {
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+export const registerUser = expressAsyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	// Validation
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error('Please include all fields');
+	}
+	res.send('Redister Route');
+});
+
+// @desc   Login user
+// @route  /api/users/login
+// @access Public
+export const loginUser = expressAsyncHandler(async (req, res) => {
 	res.send('Login Route');
-};
+});
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/userRoutes.js';
+import errorHandler from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -8,12 +9,17 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
 	res.status(201).json({ message: 'Hello World' });
 });
 
 // Routes
 app.use('/api/users', router);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server started on port: ${PORT}`);
