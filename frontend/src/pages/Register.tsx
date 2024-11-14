3a1407779b68d375415b007a57e4d69f017efa4d
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-11-14 18:57:26 +0100

    Login Form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,48 @@
+import type { ChangeEvent } from "react"
+import { useState } from "react"
+import { FaUser } from "react-icons/fa"
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Enter your name"
+            />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
